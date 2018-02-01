c75be31f0a132a97aeea096b716ec62168af77e8
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-02-02 02:28:54 +0300

    chore(Mainsale): remove marketingTokensWallet, add advisorsTokensWallet, add lotteryTokensWallet

--- contracts/Mainsale.sol
+++ contracts/Mainsale.sol
@@ -7,38 +7,50 @@ contract Mainsale is StagedCrowdsale, REPUCommonSale {
 
   address public foundersTokensWallet;
 
-  address public marketingTokensWallet;
+  address public advisorsTokensWallet;
 
   address public bountyTokensWallet;
 
+  address public lotteryTokensWallet;
+
   uint public foundersTokensPercent;
 
-  uint public marketingTokensPercent;
+  uint public advisorsTokensPercent;
 
   uint public bountyTokensPercent;
+
+  uint public lotteryTokensPercent;
 
   function setFoundersTokensPercent(uint newFoundersTokensPercent) public onlyOwner {
     foundersTokensPercent = newFoundersTokensPercent;
   }
 
-  function setMarketingTokensPercent(uint newMarketingTokensPercent) public onlyOwner {
-    marketingTokensPercent = newMarketingTokensPercent;
+  function setAdvisorsTokensPercent(uint newAdvisorsTokensPercent) public onlyOwner {
+    advisorsTokensPercent = newAdvisorsTokensPercent;
   }
 
   function setBountyTokensPercent(uint newBountyTokensPercent) public onlyOwner {
     bountyTokensPercent = newBountyTokensPercent;
   }
 
+  function setLotteryTokensPercent(uint newLotteryTokensPercent) public onlyOwner {
+    lotteryTokensPercent = newLotteryTokensPercent;
+  }
+
   function setFoundersTokensWallet(address newFoundersTokensWallet) public onlyOwner {
     foundersTokensWallet = newFoundersTokensWallet;
   }
 
-  function setMarketingTokensWallet(address newMarketingTokensWallet) public onlyOwner {
-    marketingTokensWallet = newMarketingTokensWallet;
+  function setAdvisorsTokensWallet(address newAdvisorsTokensWallet) public onlyOwner {
+    advisorsTokensWallet = newAdvisorsTokensWallet;
   }
 
   function setBountyTokensWallet(address newBountyTokensWallet) public onlyOwner {
     bountyTokensWallet = newBountyTokensWallet;
+  }
+
+  function setLotteryTokensWallet(address newLotteryTokensWallet) public onlyOwner {
+    lotteryTokensWallet = newLotteryTokensWallet;
   }
 
   function calculateTokens(uint _invested) internal returns(uint) {
@@ -53,15 +65,17 @@ contract Mainsale is StagedCrowdsale, REPUCommonSale {
   }
 
   function finish() public onlyOwner {
-    uint summaryTokensPercent = bountyTokensPercent.add(foundersTokensPercent).add(marketingTokensPercent);
+    uint summaryTokensPercent = bountyTokensPercent.add(foundersTokensPercent).add(advisorsTokensPercent).add(lotteryTokensPercent);
     uint mintedTokens = token.totalSupply();
     uint allTokens = mintedTokens.mul(percentRate).div(percentRate.sub(summaryTokensPercent));
     uint foundersTokens = allTokens.mul(foundersTokensPercent).div(percentRate);
-    uint marketingTokens = allTokens.mul(marketingTokensPercent).div(percentRate);
+    uint advisorsTokens = allTokens.mul(advisorsTokensPercent).div(percentRate);
     uint bountyTokens = allTokens.mul(bountyTokensPercent).div(percentRate);
+    uint lotteryTokens = allTokens.mul(lotteryTokensPercent).div(percentRate);
     mintTokens(foundersTokensWallet, foundersTokens);
-    mintTokens(marketingTokensWallet, marketingTokens);
+    mintTokens(advisorsTokensWallet, advisorsTokens);
     mintTokens(bountyTokensWallet, bountyTokens);
+    mintTokens(lotteryTokensWallet, lotteryTokens);
     token.finishMinting();
   }
 
