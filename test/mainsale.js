e8c16ea167aa388332f43bd981d7a85cfe86f1b5
Arkadiy A.Davydov <arkadiy.a.davydov@gmail.com>
2018-02-02 19:30:35 +0300

    chore(test): add 'devwallet' test for Mainsale

--- test/mainsale.js
+++ test/mainsale.js
@@ -1,11 +1,13 @@
 import bounty from './mainsale/bounty';
 import capped from './mainsale/capped';
 import common from './mainsale/common';
+import devwallet from './mainsale/devwallet';
 import milestonebonus from './mainsale/milestonebonus';
 import valuebonus from './mainsale/valuebonus';
 
 const token = artifacts.require('REPUToken.sol');
 const crowdsale = artifacts.require('Mainsale.sol');
+const DevWallet = artifacts.require('DevWallet.sol');
 
 contract('Mainsale - common test', function (accounts) {
   common(token, crowdsale, accounts);
@@ -25,4 +27,8 @@ contract('Mainsale - milestone bonus test', function (accounts) {
 
 contract('Mainsale - bounty test', function (accounts) {
   bounty(token, crowdsale, accounts);
+});
+
+contract('Mainsale - devWallet test', function (accounts) {
+  devwallet(token, crowdsale, DevWallet, accounts);
 });
